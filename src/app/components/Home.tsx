7701a00160cc7f92eb888d8dd9bf1dc9aedcd1d5
jingwang <jwjolie@bu.edu>
2024-12-04 01:42:14 -0500

    build

---
+++ src/app/components/Home.tsx
@@ -0,0 +1,16 @@
+import styles from './Home.module.css';
+
+export default function Home() {
+  return (
+    <section className={styles.home}>
+      <div className={styles.introText}>
+        <h1>Hello! I am Jolie Wang.</h1>
+        <div className={styles.scrollContainer}>
+          <p>
+            Software Engineer | Microsoft D365 Business Central Solution Developer
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
